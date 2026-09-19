e31e319d0ee950244458e70f786d527cf1a30799
neverdiephil <76856380+neverdiephil@users.noreply.github.com>
2026-09-19 02:44:12 -0400

    Add DeviantArt gallery data

---
+++ art-gallery/artworks.js
@@ -0,0 +1,186 @@
+const artworks = [
+  {
+    "title": "Killer Instinct Inspired Inked",
+    "page": "https://www.deviantart.com/neverdiephil/art/Killer-Instinct-Inspired-Inked-912224512"
+  },
+  {
+    "title": "Dragon Thrower 5",
+    "page": "https://www.deviantart.com/neverdiephil/art/Dragon-Thrower-5-911143308"
+  },
+  {
+    "title": "Dragon Thrower 4",
+    "page": "https://www.deviantart.com/neverdiephil/art/Dragon-Thrower-4-911143282"
+  },
+  {
+    "title": "Dragon Thrower 3",
+    "page": "https://www.deviantart.com/neverdiephil/art/Dragon-Thrower-3-911143247"
+  },
+  {
+    "title": "Dragon Thrower 2",
+    "page": "https://www.deviantart.com/neverdiephil/art/Dragon-Thrower-2-911143219"
+  },
+  {
+    "title": "Dragon Thrower 1",
+    "page": "https://www.deviantart.com/neverdiephil/art/Dragon-Thrower-1-911143175"
+  },
+  {
+    "title": "Memories",
+    "page": "https://www.deviantart.com/neverdiephil/art/Memories-911141805"
+  },
+  {
+    "title": "Breaking Chains Ink",
+    "page": "https://www.deviantart.com/neverdiephil/art/Breaking-Chains-Ink-911141771"
+  },
+  {
+    "title": "My Ghost Rider",
+    "page": "https://www.deviantart.com/neverdiephil/art/My-Ghost-Rider-911141731"
+  },
+  {
+    "title": "One Punch Ink",
+    "page": "https://www.deviantart.com/neverdiephil/art/One-Punch-Ink-911141644"
+  },
+  {
+    "title": "Nature Wave Sketch",
+    "page": "https://www.deviantart.com/neverdiephil/art/Nature-Wave-Sketch-911141562"
+  },
+  {
+    "title": "Custom Sketch 2",
+    "page": "https://www.deviantart.com/neverdiephil/art/Custom-Sketch-2-911141526"
+  },
+  {
+    "title": "Custom Character",
+    "page": "https://www.deviantart.com/neverdiephil/art/Custom-Character-911141410"
+  },
+  {
+    "title": "Samurai Spider Sketch",
+    "page": "https://www.deviantart.com/neverdiephil/art/Samurai-Spider-Sketch-911141340"
+  },
+  {
+    "title": "Killer Instinct Inspired",
+    "page": "https://www.deviantart.com/neverdiephil/art/Killer-Instinct-Inspired-911141282"
+  },
+  {
+    "title": "Dragon Ball Renaissance sketch",
+    "page": "https://www.deviantart.com/neverdiephil/art/Dragon-Ball-Renaissance-sketch-911141236"
+  },
+  {
+    "title": "My Monkey King",
+    "page": "https://www.deviantart.com/neverdiephil/art/My-Monkey-King-911141132"
+  },
+  {
+    "title": "Vegeta vs Superman Sketch",
+    "page": "https://www.deviantart.com/neverdiephil/art/Vegeta-vs-Superman-Sketch-911141082"
+  },
+  {
+    "title": "Comic Sketch",
+    "page": "https://www.deviantart.com/neverdiephil/art/Comic-Sketch-911141004"
+  },
+  {
+    "title": "Crane School Reunion",
+    "page": "https://www.deviantart.com/neverdiephil/art/Crane-School-Reunion-911140946"
+  },
+  {
+    "title": "Kid Buu Decimates Cabba",
+    "page": "https://www.deviantart.com/neverdiephil/art/Kid-Buu-Decimates-Cabba-911140797"
+  },
+  {
+    "title": "Custom Sketch",
+    "page": "https://www.deviantart.com/neverdiephil/art/Custom-Sketch-911140708"
+  },
+  {
+    "title": "Berserk Inspired 1",
+    "page": "https://www.deviantart.com/neverdiephil/art/Berserk-Inspired-1-911140624"
+  },
+  {
+    "title": "Angels and Demons",
+    "page": "https://www.deviantart.com/neverdiephil/art/Angels-and-Demons-911139546"
+  },
+  {
+    "title": "Captain Canada",
+    "page": "https://www.deviantart.com/neverdiephil/art/Captain-Canada-911139437"
+  },
+  {
+    "title": "Abstract Practice",
+    "page": "https://www.deviantart.com/neverdiephil/art/Abstract-Practice-911139269"
+  },
+  {
+    "title": "NWA style Androids 13 14 15",
+    "page": "https://www.deviantart.com/neverdiephil/art/NWA-style-Androids-13-14-15-911139005"
+  },
+  {
+    "title": "My Gohan vs Cell",
+    "page": "https://www.deviantart.com/neverdiephil/art/My-Gohan-vs-Cell-911138815"
+  },
+  {
+    "title": "DBZ Squad",
+    "page": "https://www.deviantart.com/neverdiephil/art/DBZ-Squad-911138696"
+  },
+  {
+    "title": "Akira 1988",
+    "page": "https://www.deviantart.com/neverdiephil/art/Akira-1988-911138549"
+  },
+  {
+    "title": "Berserk Inspired 2",
+    "page": "https://www.deviantart.com/neverdiephil/art/Berserk-Inspired-2-911138469"
+  },
+  {
+    "title": "Flames of Insanity",
+    "page": "https://www.deviantart.com/neverdiephil/art/Flames-of-Insanity-911138347"
+  },
+  {
+    "title": "Hiatus Break",
+    "page": "https://www.deviantart.com/neverdiephil/art/Hiatus-Break-911138212"
+  },
+  {
+    "title": "My version of Vegeta in his new form.",
+    "page": "https://www.deviantart.com/neverdiephil/art/My-version-of-Vegeta-in-his-new-form.-886208180"
+  },
+  {
+    "title": "OG Roshi vs Omni Kings",
+    "page": "https://www.deviantart.com/neverdiephil/art/OG-Roshi-vs-Omni-Kings-885988170"
+  },
+  {
+    "title": "UI Goku in western comic style",
+    "page": "https://www.deviantart.com/neverdiephil/art/UI-Goku-in-western-comic-style-885942254"
+  },
+  {
+    "title": "Roshi Kamehameha wip",
+    "page": "https://www.deviantart.com/neverdiephil/art/Roshi-Kamehameha-wip-885289131"
+  },
+  {
+    "title": "Peter been hanging with them Z boys again.",
+    "page": "https://www.deviantart.com/neverdiephil/art/Peter-been-hanging-with-them-Z-boys-again.-884890725"
+  },
+  {
+    "title": "Vegeta gut punching Jiren Redraw",
+    "page": "https://www.deviantart.com/neverdiephil/art/Vegeta-gut-punching-Jiren-Redraw-884890383"
+  },
+  {
+    "title": "Master Roshi character study 5",
+    "page": "https://www.deviantart.com/neverdiephil/art/Master-Roshi-character-study-5-884645975"
+  },
+  {
+    "title": "Master Roshi character study 4",
+    "page": "https://www.deviantart.com/neverdiephil/art/Master-Roshi-character-study-4-884645896"
+  },
+  {
+    "title": "Master Roshi character study 3",
+    "page": "https://www.deviantart.com/neverdiephil/art/Master-Roshi-character-study-3-884645825"
+  },
+  {
+    "title": "Master Roshi Character study 2",
+    "page": "https://www.deviantart.com/neverdiephil/art/Master-Roshi-Character-study-2-884645719"
+  },
+  {
+    "title": "Master Roshi character study 1",
+    "page": "https://www.deviantart.com/neverdiephil/art/Master-Roshi-character-study-1-884645639"
+  },
+  {
+    "title": "vegeta kicking the crap outta Frieza",
+    "page": "https://www.deviantart.com/neverdiephil/art/vegeta-kicking-the-crap-outta-Frieza-884645024"
+  },
+  {
+    "title": "GT Roshi got hands",
+    "page": "https://www.deviantart.com/neverdiephil/art/GT-Roshi-got-hands-884602154"
+  }
+];
